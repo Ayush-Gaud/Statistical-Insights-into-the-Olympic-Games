a390dd37e8b071698aa2e2b4dc55257549027efe
Ayush-Gaud <152425915+Ayush-Gaud@users.noreply.github.com>
2025-06-27 03:10:15 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 def preprocess(df, region_df):
-    # filtering for summer
+    # filtering for summer olympics only
     df = df[df['Season']=='Summer']
     # merge step
     df = df.merge(region_df, on='NOC', how='left')
     df.drop_duplicates(inplace=True)
-    # one-hot encoding
+    # one-hot encoding for categorical data
     df = pd.concat([df, pd.get_dummies(df['Medal']).astype(int)], axis=1)
     return df
 
